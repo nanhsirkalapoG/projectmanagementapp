3d513eadbb2c177c34ba898ef33f81e20cdcfd00
nanhsirkalapoG <togopalakrishnan@gmail.com>
2022-12-15 20:55:04 +0530

    Update db schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_27_141856) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_15_151953) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_27_141856) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "project_comments", force: :cascade do |t|
+    t.bigint "project_id"
+    t.integer "created_by"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_project_comments_on_project_id"
+  end
+
   create_table "projects", force: :cascade do |t|
     t.string "title"
     t.string "description"
@@ -35,6 +44,52 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_27_141856) do
     t.datetime "deleted_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "sprint_tasks", force: :cascade do |t|
+    t.bigint "sprint_id"
+    t.bigint "task_id"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["sprint_id"], name: "index_sprint_tasks_on_sprint_id"
+    t.index ["task_id"], name: "index_sprint_tasks_on_task_id"
+  end
+
+  create_table "sprints", force: :cascade do |t|
+    t.string "name"
+    t.datetime "starts_at"
+    t.datetime "ends_at"
+    t.integer "created_by"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "task_comments", force: :cascade do |t|
+    t.bigint "task_id"
+    t.integer "created_by"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["task_id"], name: "index_task_comments_on_task_id"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.bigint "project_id"
+    t.bigint "category_id"
+    t.integer "story_points"
+    t.datetime "due_date"
+    t.string "status"
+    t.integer "assigned_to"
+    t.integer "created_by"
+    t.integer "deleted_by"
+    t.datetime "deleted_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_tasks_on_category_id"
+    t.index ["project_id"], name: "index_tasks_on_project_id"
   end
 
   create_table "teams", force: :cascade do |t|
